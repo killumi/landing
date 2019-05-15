f9a93459221de03cd3d5863aac720d4903e9f055
fanalis <fanalis328@gmail.com>
2019-05-15 19:20:37 +0300

    commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,30 @@
 
-$(document).ready(function () {});
+$(document).ready(function () {
+    $('.js-reviews-slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        adaptiveHeight: true,
+        autoplay: true,
+    });
+
+    $(function () {
+         let users = ['40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50'];
+         let element = $('.js-online-counter');
+         setInterval(function () {
+             $(element).text(users[Math.random().toFixed(1) * 10]);
+         }, 4000);
+    });
+
+    let popup = $('.popup-overlay');
+
+    $('.js-open-popup').on('click', function () {
+        $(popup).toggleClass('active');
+    });
+
+    $('.js-popup__close-btn').on('click', function () {
+        $(popup).toggleClass('active');
+    })
+
+
+});
--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,29 @@
-"use strict";
+'use strict';
 
-$(document).ready(function () {});
+$(document).ready(function () {
+    $('.js-reviews-slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        adaptiveHeight: true,
+        autoplay: true
+    });
+
+    $(function () {
+        var users = ['40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50'];
+        var element = $('.js-online-counter');
+        setInterval(function () {
+            $(element).text(users[Math.random().toFixed(1) * 10]);
+        }, 4000);
+    });
+
+    var popup = $('.popup-overlay');
+
+    $('.js-open-popup').on('click', function () {
+        $(popup).toggleClass('active');
+    });
+
+    $('.js-popup__close-btn').on('click', function () {
+        $(popup).toggleClass('active');
+    });
+});
